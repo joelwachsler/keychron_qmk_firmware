41ed8a5ae0332fad4681992feb6cb1ab8deb2c6b
Joel Wachsler <125314669+JoelWachsler@users.noreply.github.com>
2024-12-20 22:03:41 +0100

    add custom keymap

--- keyboards/lemokey/p1_pro/iso_encoder/keymaps/via/keymap.c
+++ keyboards/lemokey/p1_pro/iso_encoder/keymaps/via/keymap.c
@@ -14,6 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "modifiers.h"
+#include "process_key_override.h"
 #include QMK_KEYBOARD_H
 #include "lemokey_common.h"
 
@@ -27,10 +29,10 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [BASE] = LAYOUT_iso_83(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,             KC_MUTE,
+        KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,             KC_MUTE,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_HOME,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                      KC_PGUP,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_PGDN,
+        KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_PGDN,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                       KC_RALT,  MO(FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -75,3 +77,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+#if defined(KEY_OVERRIDE_ENABLE)
+const key_override_t alt_n_override = ko_make_basic(MOD_MASK_ALT, KC_N, KC_DOWN);
+const key_override_t alt_p_override = ko_make_basic(MOD_MASK_ALT, KC_P, KC_UP);
+const key_override_t alt_h_override = ko_make_basic(MOD_MASK_ALT, KC_H, KC_LEFT);
+const key_override_t alt_l_override = ko_make_basic(MOD_MASK_ALT, KC_L, KC_RGHT);
+
+// const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&alt_n_override,
+	&alt_p_override,
+	&alt_h_override,
+	&alt_l_override,
+	NULL // Null terminate the array of overrides!
+};
+#endif
